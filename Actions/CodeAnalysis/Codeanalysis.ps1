694277b5fde6ffb0ed8f951da1799cb47af71b00
mazhelez <mazhelez@microsoft.com>
2023-09-12 01:26:28 +0200

    Filter errorLog files properly

--- Actions/CodeAnalysis/Codeanalysis.ps1
+++ Actions/CodeAnalysis/Codeanalysis.ps1
@@ -5,9 +5,9 @@ param(
 
 Import-Module $PSScriptRoot/ErrorlogToSarif.psm1
 
-$logfiles = Get-ChildItem -Path $Path -Filter errorLog.json -Recurse
+$logfiles = Get-ChildItem -Path $Path -Filter "*-errorLog.json" -Recurse
 Write-Host "Found $($logfiles) errorlog.Json files"
 $logfiles | ForEach-Object {
     Write-Host "Processing $($_.FullName)"
     ConvertTo-SarifLog -Path $_.FullName -OutputPath $OutputPath
-} 
+}
